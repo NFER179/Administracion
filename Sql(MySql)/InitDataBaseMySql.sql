546949ba92d728a123fc172853669504168a5091
n.fernandez <nikin179@hotmail.com>
2017-10-11 17:02:24 -0300

    Add files via upload

--- Sql(MySql)/InitDataBaseMySql.sql
+++ Sql(MySql)/InitDataBaseMySql.sql
@@ -8,7 +8,7 @@ create table if not exists config(nombre_gimnasio varchar(50) not null);
 
 insert into config(nombre_gimnasio) values("Lo de Betty");
 
-create table if not exists usuario(id_usuario varchar(6) not null
+create table if not exists cliente(id_cliente varchar(5) not null
 		, nombres varchar(50) not null
 		, apellido varchar(30) not null
 		, nacionalidad varchar(50) not null
@@ -17,7 +17,7 @@ create table if not exists usuario(id_usuario varchar(6) not null
 		, fecha_nacimiento date
 		, ciudad varchar(30) not null
 		, localidad varchar(30) not null
-		, primary key (id_usuario));
+		, primary key (id_cliente));
 		
-insert into usuario(id_usuario, nombres, apellido, nacionalidad, tipo_doc, num_documento, fecha_nacimiento, ciudad, localidad)
-values('001327', 'Nicolas Daniel', 'Fernandez', 'Argentino', 'DNI', '20-34182304-2', STR_TO_DATE('09-9-1988','%d-%m-%Y'), 'Moreno', 'Moreno');
+insert into cliente(id_cliente, nombres, apellido, nacionalidad, tipo_doc, num_documento, fecha_nacimiento, ciudad, localidad)
+values('13254', 'Nicolas Daniel', 'Fernandez', 'Argentino', 'DNI', '20-34179142-2', STR_TO_DATE('02-11-1988','%d-%m-%Y'), 'Moreno', 'Moreno');
